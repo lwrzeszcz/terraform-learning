659f2b88cce8c4f30499fe663168b17e170c6c6a
lzwz <lukasz.wrzeszcz@gft.com>
2019-02-20 17:27:09 +0100

    gcp kubernetes cluster setup

---
+++ gcp_kubernetes.tf
@@ -0,0 +1,31 @@
+resource "google_container_cluster" "kubey" {
+    name = "kubeycluster"
+    zone = "europe-west3-a"
+    initial_node_count = "1"
+
+    additional_zones = [
+        "europe-west3-b",
+        "europe-west3-c"
+        ]
+
+    master_auth {
+        password = "password"
+        username = "username"
+    }
+
+    node_config {
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/compute",
+            "https://www.googleapis.com/auth/devstorage.read_only",
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring"
+        ]
+
+        labels {
+            this-is-for = "dev-cluster"
+        }
+
+        tags = ["dev","work"]
+    }
+}
+
